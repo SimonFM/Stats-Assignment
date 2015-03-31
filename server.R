abef0a98f5c08323b873799b2095e571bef4f01c
Simon <markhams@tcd.ie>
2015-03-31 15:07:18 +0100

    Fixed things... yeah

--- server.R
+++ server.R
@@ -1,4 +1,22 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
 library(shiny)
+library(XLConnect)
 
 shinyServer(function(input, output) {
+
+  output$distPlot <- renderPlot({
+    wb <- loadWorkbook("WorldCupPlayerInfo.xlsx",create = F)
+    data = readWorksheet(wb,sheet = 1)
+    
+    barplot(table(data$League))
+    
+    #scatterplot(table(data$Club))
+  })
+
 })
--- ui.R
+++ ui.R
@@ -1,13 +1,24 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
 library(shiny)
 
-
 shinyUI(fluidPage(
-  
-  titlePanel("title panel"),
-  
+
+  # Application title
+  titlePanel("World Cup Data"),
+
+  # Sidebar with a slider input for number of bins
   sidebarLayout(
-    sidebarPanel( "sidebar panel"),
-    mainPanel("main panel")
+    sidebarPanel("Distribution of Players per League"),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
   )
-  
 ))
